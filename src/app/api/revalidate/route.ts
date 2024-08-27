6b10bc22bff38ef792498b115eb03cb52aeba26e
Mansoor Roeen <mansoorroeen71@gmail.com>
2024-08-27 14:51:05 +0530

    Removed logs

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
 
     const result = await request.json();
-    console.log(result)
     const webhookEvent = result?.event
 
     if (!webhookEvent) {
